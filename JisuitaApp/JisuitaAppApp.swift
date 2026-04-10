5fd4bd4ff8db49be267afedc3825a16ce800cb20
オフィス岳 <info@office-gaku.net>
2026-04-10 16:15:40 +0900

    Initial Commit

---
+++ JisuitaApp/JisuitaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JisuitaAppApp.swift
+//  JisuitaApp
+//
+//  Created by 株式会社オフィス岳 on 2026/04/10.
+//
+
+import SwiftUI
+
+@main
+struct JisuitaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
